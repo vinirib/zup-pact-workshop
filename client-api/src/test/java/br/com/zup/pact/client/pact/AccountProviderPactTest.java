6c8dda6d653cffe1475f2851b07909ad3d92dc4d
viniciusribeirogtk <viniciusribeirosp@gmail.com>
2020-03-09 14:26:28 -0300

    Added more tests Created More endpoints to client and account

--- client-api/src/test/java/br/com/zup/pact/client/pact/AccountProviderPactTest.java
+++ client-api/src/test/java/br/com/zup/pact/client/pact/AccountProviderPactTest.java
@@ -11,6 +11,7 @@ import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import br.com.zup.pact.client.dto.BalanceDTO;
 import br.com.zup.pact.client.integration.account.service.AccountIntegrationService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,12 +20,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
-@Provider("AccountProvider")
+@Provider("AccountBalanceProvider")
 @PactBroker(host = "localhost", port = "80")
 @VerificationReports
 @ExtendWith(SpringExtension.class)
@@ -32,7 +34,7 @@ import static org.mockito.BDDMockito.given;
 public class AccountProviderPactTest {
 
     @LocalServerPort
-    int localServerPort;
+    private int localServerPort;
 
     @MockBean
     private AccountIntegrationService accountIntegrationService;
@@ -42,14 +44,26 @@ public class AccountProviderPactTest {
         context.setTarget(new HttpTestTarget("localhost", localServerPort, "/"));
     }
 
+    @BeforeAll
+    static void enablePublishingPact() {
+        System.setProperty("pact.verifier.publishResults", "true");
+    }
+
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
     void testTemplate(PactVerificationContext context) {
         context.verifyInteraction();
     }
 
-    @State("get an account")
-    public void uniqueId() {
-        given(accountIntegrationService.getBalance(eq(1))).willReturn(Optional.of(new BalanceDTO()));
+    @State("get balance by account id")
+    public void getBalanceDTO() {
+        final BalanceDTO balanceDTO = BalanceDTO.builder().clientId(1).accountId(1).balance(new BigDecimal("100")).build();
+        given(accountIntegrationService.getBalance(eq(1))).willReturn(Optional.of(balanceDTO));
+        //final Optional<BalanceDTO> balance = accountIntegrationService.getBalance(1);
+    }
+
+    @State("No accounts exist")
+    public void getBalanceDTONotWorking() {
+        given(accountIntegrationService.getBalance(eq(1))).willReturn(Optional.empty());
     }
 }
